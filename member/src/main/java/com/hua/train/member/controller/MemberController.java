2a7348e835d925dc612cdddc0d7fcb6590f4d987
ShuKun Li <2562017477@qq.com>
2024-02-04 20:06:16 +0800

    注册接口实现

--- member/src/main/java/com/hua/train/member/controller/MemberController.java
+++ member/src/main/java/com/hua/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.hua.train.member.controller;
 import com.hua.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,11 @@ public class MemberController {
   public Integer count(){
     return memberService.count();
   }
+
+  @PostMapping ("/register")
+  public long register(String mobile){
+    return memberService.register(mobile);
+  }
+
 
 }
--- member/src/main/java/com/hua/train/member/service/MemberService.java
+++ member/src/main/java/com/hua/train/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.hua.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.hua.train.member.domain.Member;
+import com.hua.train.member.domain.MemberExample;
 import com.hua.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,4 +17,23 @@ public class MemberService {
   public int count(){
     return Math.toIntExact(memberMapper.countByExample(null));
   }
+
+  public long register(String mobile){
+    MemberExample memberExample = new MemberExample();
+    memberExample.createCriteria().andMobileEqualTo(mobile);
+    List<Member> list = memberMapper.selectByExample(memberExample);
+
+    if(CollUtil.isNotEmpty(list)){
+      //return list.get(0).getId();
+    throw new RuntimeException("手机号已注册");
+    }
+
+    Member member = new Member();
+    member.setId(System.currentTimeMillis());
+    member.setMobile(mobile);
+
+    memberMapper.insert(member);
+    return member.getId();
+  }
+
 }
